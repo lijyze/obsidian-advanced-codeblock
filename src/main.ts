dadbfa4b729f87ead47888ba70d6b0dbaa126793
lijyze <lijyze@outlook.com>
2022-06-01 21:04:02 +0800

    remove thing about setting

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { Editor, Plugin } from "obsidian";
 import { commonCodeblockPostProcessor } from "./postProcessor";
 
 // Remember to rename these classes and interfaces!
@@ -7,21 +7,12 @@ interface MyPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
-};
-
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 	private editor: Editor;
 
 	async onload() {
 		console.log("Loading Advanced Codeblock");
-
-		await this.loadSettings();
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// Handle every code blocks
 		this.registerMarkdownPostProcessor((element, context) => {
@@ -30,47 +21,4 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						console.log("Secret: " + value);
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
 }
